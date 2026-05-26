e919c02fe283e8cd7486b59aa07db9ea54e14a09
lamifaharia <lamifaharia@gmail.com>
2026-05-27 01:24:02 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-red-600 text-center mt-20">
+        Blood Donation App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
